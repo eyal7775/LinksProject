6864ac974c7631af56f1341b9b0ae78685cde2e4
eyal7775 <eyal.naftalovich@mdclone.com>
2022-08-24 11:35:17 +0300

    khxdfkjghkjxhfkzh

---
+++ arieth.py
@@ -0,0 +1,14 @@
+List1 = [7,3,8,2,6,10]
+List1.reverse()
+print("The reversed list is ", List1)
+
+List2 = [54,23,98,85,13,75,23,99,34,14,4,27]
+List2.sort()
+print("The sorted list is ", List2)
+
+List3 = []
+List3 = List1.copy()
+print("List3 = ", List3)
+
+List4 = List2[2:6]
+print("The index values for List2 are", List4)
