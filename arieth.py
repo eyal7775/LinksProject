637057515dea3eaf4fa78e33f3c8a96c13881a2b
eyal7775 <eyal.naftalovich@mdclone.com>
2022-08-30 10:54:18 +0300

    57657548

--- arieth.py
+++ arieth.py
@@ -1,12 +1,12 @@
-List1 = [7,3,8,2,6,10]
+""" this is test for program """
+List1 = [7, 3, 8, 2, 6, 10]
 List1.reverse()
 print("The reversed list is ", List1)
 
-List2 = [54,23,98,85,13,75,23,99,34,14,4,27]
+List2 = [54, 23, 98, 85, 13, 75, 23, 99, 34, 14, 4, 27]
 List2.sort()
 print("The sorted list is ", List2)
 
-List3 = []
 List3 = List1.copy()
 print("List3 = ", List3)
 
